0ca0699e36a6dbb400542c7d80ada17cb386e8a2
777-spec-pan <penfood777@gmail.com>
2026-07-21 10:23:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,43 @@
-const C='otsu4-v3-20260721',A=['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(A)))});self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const VERSION='otsu4-v3.1-20260721';
+const ASSETS=['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==VERSION).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const request=event.request;
+  if(request.mode==='navigate'){
+    event.respondWith(
+      fetch(request,{cache:'no-store'})
+        .then(response=>{
+          const copy=response.clone();
+          caches.open(VERSION).then(cache=>cache.put('./index.html',copy));
+          return response;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(request,{cache:'no-store'})
+      .then(response=>{
+        if(request.method==='GET' && response.ok){
+          const copy=response.clone();
+          caches.open(VERSION).then(cache=>cache.put(request,copy));
+        }
+        return response;
+      })
+      .catch(()=>caches.match(request))
+  );
+});
