327074038c9d85f2490017650b9731956dcc9d65
777-spec-pan <penfood777@gmail.com>
2026-07-21 09:29:21 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='otsu4-final-v1',A=['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
