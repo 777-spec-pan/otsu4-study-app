0c93b0841043dab35174e895dc01b2b8e3c917d2
777-spec-pan <penfood777@gmail.com>
2026-07-21 10:42:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='otsu4-master-v4-20260721';
+const CACHE='otsu4-master-v5-500q-20260721';
 const ASSETS=['./','./index.html','./questions.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
